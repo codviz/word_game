09ce49c9206e113fe09db14d191e3765b320ad52
codviz <89945886+codviz@users.noreply.github.com>
2024-07-09 13:19:19 -0400

    Update analysis_dictionary_make.py

--- analysis_dictionary_make.py
+++ analysis_dictionary_make.py
@@ -1,6 +1,5 @@
-
 def create_dict():
-
+  
     most_used_file = open(r"data/most_used_words.txt","r")
     most_used_content = most_used_file.read()
     most_used_list = most_used_content.split()
@@ -13,6 +12,8 @@ def create_dict():
     while True:
         if analysis_words_file.readline():
             line_list = line.split(',')
+            while 'NA' in line_list: line_list.remove('NA')
+            while "NA\n" in line_list: line_list.remove('NA\n')
             if int(line_list[3]) > 2:
                 if line_list[1] in most_used_list:
                     analysis_words_dictionary[line_list[1]] = line_list[2:]
